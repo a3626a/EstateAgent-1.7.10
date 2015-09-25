e63f280f418671af9459626428524305f07bdf8f
a3626a <a3626a@naver.com>
2015-09-26 07:28:04 +0900

    improve render algorithm to prevent Z-Fighting

--- src/main/java/oortcloud/estateagent/hadler/MinecraftForgeEventHandler.java
+++ src/main/java/oortcloud/estateagent/hadler/MinecraftForgeEventHandler.java
@@ -35,7 +35,7 @@ public class MinecraftForgeEventHandler {
 			ArrayList<ChunkCoordIntPairWithDimension> list = ChunkManager.list.get(player.getCommandSenderName());
 			if (list == null)
 				return;
-			
+
 			Tessellator tessellator = Tessellator.instance;
 			float partialTickTime = event.partialTicks;
 			int dim = Minecraft.getMinecraft().theWorld.provider.dimensionId;
@@ -43,7 +43,7 @@ public class MinecraftForgeEventHandler {
 			double px = player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTickTime;
 			double py = player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTickTime;
 			double pz = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTickTime;
-			
+
 			GL11.glEnable(GL11.GL_BLEND);
 			GL11.glDisable(GL11.GL_CULL_FACE);
 			GL11.glDisable(GL11.GL_LIGHTING);
@@ -51,54 +51,99 @@ public class MinecraftForgeEventHandler {
 			GL11.glShadeModel(GL11.GL_SMOOTH);
 			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 			GL11.glDepthMask(false);
-			
+
 			GL11.glPushMatrix();
 			GL11.glTranslated(-px, 0, -pz);
 
 			int num = 64;
-			float dx = 0.25F;
-			
+			float offset = 0.1F;
+
 			for (ChunkCoordIntPairWithDimension i : list) {
 				if (i.dim == dim) {
 					GL11.glPushMatrix();
 					GL11.glTranslatef(i.chunkXPos * 16, 0, i.chunkZPos * 16);
-					if (!hasChunk(list, dim, i.chunkXPos, i.chunkZPos - 1)) {
+					boolean hasChunkSouth = hasChunk(list, dim, i.chunkXPos, i.chunkZPos + 1);
+					boolean hasChunkWest = hasChunk(list, dim, i.chunkXPos - 1, i.chunkZPos);
+					boolean hasChunkNorth = hasChunk(list, dim, i.chunkXPos, i.chunkZPos - 1);
+					boolean hasChunkEast = hasChunk(list, dim, i.chunkXPos + 1, i.chunkZPos);
+					if (!hasChunkNorth) {
 						GL11.glBegin(GL11.GL_TRIANGLE_STRIP);
+						float start = 0;
+						float length = 16;
+						if (!hasChunkWest) {
+							start = offset;
+							length -= offset;
+						}
+						if (!hasChunkEast) {
+							length -= offset;
+						}
+						float dx = length / (float) num;
 						for (int j = 0; j <= num; j++) {
 							GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-							GL11.glVertex3d(dx * j, -py, 0);
+							GL11.glVertex3d(start + dx * j, -py, offset);
 							GL11.glColor4f(1.0F, 1.0F, 1.0F, 0.1F);
-							GL11.glVertex3d(dx * j, 4 + 4 * Math.sin((16 * Math.PI / 64.0) * dx * j - (Minecraft.getMinecraft().theWorld.getWorldTime() + partialTickTime) / 10.0), 0);
+							GL11.glVertex3d(start + dx * j, 4 + 4 * Math.sin((4 * Math.PI / 64.0) * j - (Minecraft.getMinecraft().theWorld.getWorldTime() + partialTickTime) / 10.0), offset);
+						}
+
+						GL11.glEnd();
+					}
+					if (!hasChunkEast) {
+						GL11.glBegin(GL11.GL_TRIANGLE_STRIP);
+						float start = 0;
+						float length = 16;
+						if (!hasChunkNorth) {
+							start = offset;
+							length -= offset;
+						}
+						if (!hasChunkSouth) {
+							length -= offset;
+						}
+						float dx = length / (float) num;
+						for (int j = 0; j <= num; j++) {
+							GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+							GL11.glVertex3d(16 - offset, -py, start + dx * j);
+							GL11.glColor4f(1.0F, 1.0F, 1.0F, 0.1F);
+							GL11.glVertex3d(16 - offset, 4 + 4 * Math.sin(4 * Math.PI + (4 * Math.PI / 64.0) * j - (Minecraft.getMinecraft().theWorld.getWorldTime() + partialTickTime) / 10.0), start + dx * j);
 						}
 						GL11.glEnd();
 					}
-					if (!hasChunk(list, dim, i.chunkXPos + 1, i.chunkZPos)) {
+					if (!hasChunkSouth) {
 						GL11.glBegin(GL11.GL_TRIANGLE_STRIP);
+						float start = 0;
+						float length = 16;
+						if (!hasChunkEast) {
+							start = offset;
+							length -= offset;
+						}
+						if (!hasChunkWest) {
+							length -= offset;
+						}
+						float dx = length / (float) num;
 						for (int j = 0; j <= num; j++) {
 							GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-							GL11.glVertex3d(16, -py, dx * j);
+							GL11.glVertex3d(16 - start - dx * j, -py, 16 - offset);
 							GL11.glColor4f(1.0F, 1.0F, 1.0F, 0.1F);
-							GL11.glVertex3d(16, 4 + 4 * Math.sin(4 * Math.PI + (16 * Math.PI / 64.0) * dx * j - (Minecraft.getMinecraft().theWorld.getWorldTime() + partialTickTime) / 10.0), dx * j);
+							GL11.glVertex3d(16 - start - dx * j, 4 + 4 * Math.sin(8 * Math.PI + (4 * Math.PI / 64.0) * j - (Minecraft.getMinecraft().theWorld.getWorldTime() + partialTickTime) / 10.0), 16 - offset);
 						}
 						GL11.glEnd();
 					}
-					if (!hasChunk(list, dim, i.chunkXPos, i.chunkZPos + 1)) {
+					if (!hasChunkWest) {
 						GL11.glBegin(GL11.GL_TRIANGLE_STRIP);
-						for (int j = 0; j <= num; j++) {
-							GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-							GL11.glVertex3d(16 - dx * j, -py, 16);
-							GL11.glColor4f(1.0F, 1.0F, 1.0F, 0.1F);
-							GL11.glVertex3d(16 - dx * j, 4 + 4 * Math.sin(8 * Math.PI + (16 * Math.PI / 64.0) * dx * j - (Minecraft.getMinecraft().theWorld.getWorldTime() + partialTickTime) / 10.0), 16);
+						float start = 0;
+						float length = 16;
+						if (!hasChunkSouth) {
+							start = offset;
+							length -= offset;
 						}
-						GL11.glEnd();
-					}
-					if (!hasChunk(list, dim, i.chunkXPos - 1, i.chunkZPos)) {
-						GL11.glBegin(GL11.GL_TRIANGLE_STRIP);
+						if (!hasChunkNorth) {
+							length -= offset;
+						}
+						float dx = length / (float) num;
 						for (int j = 0; j <= num; j++) {
 							GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-							GL11.glVertex3d(0, -py, 16 - dx * j);
+							GL11.glVertex3d(offset, -py, 16 - start - dx * j);
 							GL11.glColor4f(1.0F, 1.0F, 1.0F, 0.1F);
-							GL11.glVertex3d(0, 4 + 4 * Math.sin(12 * Math.PI + (16 * Math.PI / 64.0) * dx * j - (Minecraft.getMinecraft().theWorld.getWorldTime() + partialTickTime) / 10.0), 16 - dx * j);
+							GL11.glVertex3d(offset, 4 + 4 * Math.sin(12 * Math.PI + (4 * Math.PI / 64.0) * j - (Minecraft.getMinecraft().theWorld.getWorldTime() + partialTickTime) / 10.0), 16 - start - dx * j);
 						}
 						GL11.glEnd();
 					}
@@ -121,7 +166,7 @@ public class MinecraftForgeEventHandler {
 		}
 		return false;
 	}
-	
+
 	@SubscribeEvent
 	public void onEntityConstructing(EntityConstructing event) {
 		if (event.entity instanceof EntityPlayer) {
